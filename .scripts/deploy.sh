326a010d1ab51c7421b2adaeb4e03ededfdbe242
akingofcrows <xmusashi@gmail.com>
2020-12-27 00:07:17 -0500

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -13,6 +13,6 @@ git commit -m "Deploy ${TRAVIS_COMMIT}"
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "ssh://${GH_DEPLOY_KEY}@${GH_REF}" master:dist
+git push --force "ssh://git@github.com:akingofcrows/Hangout.git" master:dist
 
 echo 'Deployed on dist branch'
